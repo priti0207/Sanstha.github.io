b4bbd9e0a2257567df4ee6a436dc521a6b21842e
priti0207 <96966006+priti0207@users.noreply.github.com>
2024-02-23 16:01:37 +0530

    Update header_footer.js

--- js/header_footer.js
+++ js/header_footer.js
@@ -9,3 +9,22 @@ fetch('header.html')
         fetch('footer.html')
         .then(response => response.text())
         .then(text => document.getElementById('includedContent1').innerHTML = text);
+
+
+
+    function checkAndOpenPopup() {
+        var name = document.getElementById('name').value;
+        var phoneNumber = document.getElementById('phone').value;
+
+        // Check if the name is not empty and if the phone number is 10 digits
+        if (name.trim() !== "" && phoneNumber.length === 10) {
+            document.getElementById('overlay').style.display = 'block';
+        } else {
+            alert("Please enter a valid name and a valid 10-digit phone number.");
+        }
+    }
+
+    function closePopup() {
+        document.getElementById('overlay').style.display = 'none';
+    }
+
